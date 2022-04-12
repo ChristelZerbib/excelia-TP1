5f15bb7697334b9158c9d63ea4972845dbd139dd
ChristelZerbib <66353707+ChristelZerbib@users.noreply.github.com>
2022-04-12 15:05:36 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -5,6 +5,6 @@ def hello():
     Voici un exemple de la fonction hello value
 
     >>> hello()
-    'HHello World ! On essaie la merge request pour la troisième fois !'
+    'HHello World ! On essaie la merge request pour la X fois !'
     """
-	return "Hello World ! On essaie la merge request pour la troisième fois !"
+	return "Hello World ! On essaie la merge request pour la X fois !"
