5587139b081dad92ad570fb9d755d789a23bba40
Christel Zerbib <christelzerbib@gmail.com>
2022-04-12 09:20:46 +0200

    add tests

---
+++ helpers.py
@@ -0,0 +1,10 @@
+
+
+def hello():
+	"""
+    Voici un exemple de la fonction hello value
+
+    >>> hello()
+    Hello World !
+    """
+	return "Hello World !"
--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 from bottle import route, run
 import sys
+import helpers
 
 
 @route('/')
 def index():
-    return "Hello World!"
+    texte = hello()
+    return texte
 
 
 run(host="0.0.0.0", port=sys.argv[1], reloader=True)
